f5f9f0f2a79c60c31703e69e57c64d5da0edd6f1
pavantarigopula <127600516+pavantarigopula@users.noreply.github.com>
2023-03-15 14:16:48 +0530

    Delete src directory

--- src/tests/UITests/test_signuppage.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from src.Pages.signup_page import *
-
-@pytest.mark.smoke
-@pytest.mark.regression
-def test_signup():
-    result  = signup()
-    assert result == "https://app.splashthat.com/login/verifyEmail"
-
